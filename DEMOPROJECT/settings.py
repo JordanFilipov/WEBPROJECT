0a8b683e9269a9456aada80ad093eb3a24316690
Jordan Filipov <jordanfilipov@yahoo.com>
2020-04-11 21:07:08 +0200

    Ready for deployment to heroku

--- DEMOPROJECT/settings.py
+++ DEMOPROJECT/settings.py
@@ -131,19 +131,21 @@ BOOTSTRAP3 = {
 }
 
 #Heroku settings
-if os.getcwd() == '/app':
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(default='postgress://localhost')
-    }
 
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+import dj_database_url
 
-    ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
